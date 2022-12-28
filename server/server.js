c31d5ca097f42199f97b683b53ef30271ed3b2b5
Mark Zvenigorodsky <mark.zvenigorodsky@gmail.com>
2022-12-28 19:53:43 +0200

    Added launch file, and fixed db connection problems

--- server/server.js
+++ server/server.js
@@ -1,5 +1,3 @@
-require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` })
-
 const express = require("express")
 const app = express()
 const Router = require("./Routes/index")
@@ -8,30 +6,30 @@ const path = require("path")
 const cors = require("cors")
 
 const MONGO_URI = () => {
-  return `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_USER_PWD}@localhost:${process.env.DB_PORT}/${process.env.MONGO_DB_NAME}`
+    return `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_USER_PWD}@127.0.0.1:${process.env.DB_PORT}/${process.env.MONGO_DB_NAME}`
 }
 
 const runDB = async () => {
-  await utils.connectToDB(MONGO_URI())
+    await utils.connectToDB(MONGO_URI())
 }
 
 const configureServer = async () => {
-  await runDB()
-  app.use(cors())
-  app.use(express.json())
+    await runDB()
+    app.use(cors())
+    app.use(express.json())
 
-  app.use("/api/v1", Router)
+    app.use("/api/v1", Router)
 
-  app.use(express.static(path.join(__dirname, "..", "dist")))
+    app.use(express.static(path.join(__dirname, "..", "dist")))
 
-  app.get("/", (req, res, next) => {
-    res.sendFile(path.join(__dirname, "..", "dist", "index.html"))
-  })
+    app.get("/", (req, res, next) => {
+        res.sendFile(path.join(__dirname, "..", "dist", "index.html"))
+    })
 }
 
 const startServer = port =>
-  app.listen(port, () => {
-    console.log(`server listening on port ${port}...`)
-  })
+    app.listen(port, () => {
+        console.log(`server listening on port ${port}...`)
+    })
 
 module.exports = { startServer, configureServer, app, MONGO_URI }
